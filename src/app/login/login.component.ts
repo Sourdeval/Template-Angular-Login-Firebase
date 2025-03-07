56da6594f081dee93778c4de786547477fef6748
Sourdeval <sourdeval@laposte.net>
2025-03-07 03:49:50 +0100

    Add icons in login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'talf-login',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
